6121a47ad24745ff0ef4041c1a9705c2d0ce3d32
Garrett <emrickgarrett@gmail.com>
2013-10-04 23:35:53 -0400

    changes

--- Android/src/com/example/handsonandroid/FeedActivity.java
+++ Android/src/com/example/handsonandroid/FeedActivity.java
@@ -16,7 +16,7 @@ public class FeedActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_feed);
 	}
 
 	@Override

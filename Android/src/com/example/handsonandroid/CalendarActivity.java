8938b479ea87858e85d87efc197cd36742e95e49
Garrett <emrickgarrett@gmail.com>
2013-10-05 03:22:21 -0400

    Fix build

--- Android/src/com/example/handsonandroid/CalendarActivity.java
+++ Android/src/com/example/handsonandroid/CalendarActivity.java
@@ -84,117 +84,117 @@ public class CalendarActivity extends Activity {
 
   gridview.setOnItemClickListener(new OnItemClickListener() {
    public void onItemClick(AdapterView parent, View v,
-     int position, long id) {
-    // removing the previous view if added
-    if (((LinearLayout) rLayout).getChildCount() > 0) {
-     ((LinearLayout) rLayout).removeAllViews();
-    }
-    desc = new ArrayList();
-    date = new ArrayList();
-    ((CalendarAdapter) parent.getAdapter()).setSelected(v);
-    String selectedGridDate = CalendarAdapter.dayString
-      .get(position);
-    String[] separatedTime = selectedGridDate.split("-");
-    String gridvalueString = separatedTime[2].replaceFirst("^0*",
-      "");// taking last part of date. ie; 2 from 2012-12-02.
-    int gridvalue = Integer.parseInt(gridvalueString);
-    // navigate to next or previous month on clicking offdays.
-    if ((gridvalue > 10) && (position < 8)) {
-     setPreviousMonth();
-     refreshCalendar();
-    } else if ((gridvalue < 7) && (position > 28)) {
-     setNextMonth();
-     refreshCalendar();
-    }
-    ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+	 int position, long id) {
+	   // removing the previous view if added
+	   if (((LinearLayout) rLayout).getChildCount() > 0) {
+		   ((LinearLayout) rLayout).removeAllViews();
+	   }
+	   desc = new ArrayList();
+	   date = new ArrayList();
+	   ((CalendarAdapter) parent.getAdapter()).setSelected(v);
+	   String selectedGridDate = CalendarAdapter.dayString
+			   .get(position);
+	   String[] separatedTime = selectedGridDate.split("-");
+	   String gridvalueString = separatedTime[2].replaceFirst("^0*",
+			   "");// taking last part of date. ie; 2 from 2012-12-02.
+	   int gridvalue = Integer.parseInt(gridvalueString);
+	   // navigate to next or previous month on clicking offdays.
+	   if ((gridvalue > 10) && (position < 8)) {
+		   setPreviousMonth();
+		   refreshCalendar();
+	   } else if ((gridvalue < 7) && (position > 28)) {
+		   setNextMonth();
+		   refreshCalendar();
+	   }
+	   ((CalendarAdapter) parent.getAdapter()).setSelected(v);
 
-    for (int i = 0; i < CalendarUtility.startDates.size(); i++) {
-     if (CalendarUtility.startDates.get(i).equals(selectedGridDate)) {
-      desc.add(CalendarUtility.nameOfEvent.get(i));
-     }
-    }
+	   for (int i = 0; i < CalendarUtility.startDates.size(); i++) {
+		   if (CalendarUtility.startDates.get(i).equals(selectedGridDate)) {
+			   desc.add(CalendarUtility.nameOfEvent.get(i));
+		   }
+	   }
 
-    if (desc.size() > 0) {
-     for (int i = 0; i < desc.size(); i++) {
-      TextView rowTextView = new TextView(CalendarActivity.this);
+	   if (desc.size() > 0) {
+		   for (int i = 0; i < desc.size(); i++) {
+			   TextView rowTextView = new TextView(CalendarActivity.this);
 
-      // set some properties of rowTextView or something
-      rowTextView.setText("Event:" + desc.get(i));
-      rowTextView.setTextColor(Color.BLACK);
+			   // set some properties of rowTextView or something
+			   rowTextView.setText("Event:" + desc.get(i));
+			   rowTextView.setTextColor(Color.BLACK);
 
-      // add the textview to the linearlayout
-      rLayout.addView(rowTextView);
+			   // add the textview to the linearlayout
+			   rLayout.addView(rowTextView);
+			   
+		   }
 
-     }
+	   }
 
-    }
+	   desc = null;
 
-    desc = null;
+   	}
 
-   }
-
-  });
+  	});
  }
 
- protected void setNextMonth() {
-  if (month.get(GregorianCalendar.MONTH) == month
-    .getActualMaximum(GregorianCalendar.MONTH)) {
-   month.set((month.get(GregorianCalendar.YEAR) + 1),
-     month.getActualMinimum(GregorianCalendar.MONTH), 1);
-  } else {
-   month.set(GregorianCalendar.MONTH,
-     month.get(GregorianCalendar.MONTH) + 1);
-  }
+ 	protected void setNextMonth() {
+ 		if (month.get(GregorianCalendar.MONTH) == month
+ 				.getActualMaximum(GregorianCalendar.MONTH)) {
+ 			month.set((month.get(GregorianCalendar.YEAR) + 1),
+ 					month.getActualMinimum(GregorianCalendar.MONTH), 1);
+ 		} else {
+ 			month.set(GregorianCalendar.MONTH,
+ 					month.get(GregorianCalendar.MONTH) + 1);
+ 		}
 
- }
+ 	}
 
- protected void setPreviousMonth() {
-  if (month.get(GregorianCalendar.MONTH) == month
-    .getActualMinimum(GregorianCalendar.MONTH)) {
-   month.set((month.get(GregorianCalendar.YEAR) - 1),
-     month.getActualMaximum(GregorianCalendar.MONTH), 1);
-  } else {
-   month.set(GregorianCalendar.MONTH,
-     month.get(GregorianCalendar.MONTH) - 1);
-  }
+ 	protected void setPreviousMonth() {
+ 		if (month.get(GregorianCalendar.MONTH) == month
+ 				.getActualMinimum(GregorianCalendar.MONTH)) {
+ 			month.set((month.get(GregorianCalendar.YEAR) - 1),
+ 					month.getActualMaximum(GregorianCalendar.MONTH), 1);
+ 		} else {
+ 			month.set(GregorianCalendar.MONTH,
+ 					month.get(GregorianCalendar.MONTH) - 1);
+ 		}
 
- }
+ 	}
 
- protected void showToast(String string) {
-  Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+ 	protected void showToast(String string) {
+ 		Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
 
- }
+ 	}
 
- public void refreshCalendar() {
-  TextView title = (TextView) findViewById(R.id.title);
+ 	public void refreshCalendar() {
+ 		TextView title = (TextView) findViewById(R.id.title);
 
-  adapter.refreshDays();
-  adapter.notifyDataSetChanged();
-  handler.post(calendarUpdater); // generate some calendar items
+ 		adapter.refreshDays();
+ 		adapter.notifyDataSetChanged();
+ 		handler.post(calendarUpdater); // generate some calendar items
 
-  title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
- }
+ 		title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+ 	}
 
- public Runnable calendarUpdater = new Runnable() {
+ 	public Runnable calendarUpdater = new Runnable() {
 
-  @Override
-  public void run() {
-   items.clear();
+ 		@Override
+ 		public void run() {
+ 			items.clear();
 
-   // Print dates of the current week
-   DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
-   String itemvalue;
-   event = CalendarUtility.readCalendarEvent(CalendarActivity.this);
-   Log.d("=====Event====", event.toString());
-   Log.d("=====Date ARRAY====", CalendarUtility.startDates.toString());
+ 			// Print dates of the current week
+ 			DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+ 			String itemvalue;
+ 			event = CalendarUtility.readCalendarEvent(CalendarActivity.this);
+ 			Log.d("=====Event====", event.toString());
+ 			Log.d("=====Date ARRAY====", CalendarUtility.startDates.toString());
 
-   for (int i = 0; i < CalendarUtility.startDates.size(); i++) {
-    itemvalue = df.format(itemmonth.getTime());
-    itemmonth.add(GregorianCalendar.DATE, 1);
-    items.add(CalendarUtility.startDates.get(i).toString());
-   }
-   adapter.setItems(items);
-   adapter.notifyDataSetChanged();
-  }
- };
+ 			for (int i = 0; i < CalendarUtility.startDates.size(); i++) {
+ 				itemvalue = df.format(itemmonth.getTime());
+ 				itemmonth.add(GregorianCalendar.DATE, 1);
+ 				items.add(CalendarUtility.startDates.get(i).toString());
+ 			}
+ 			adapter.setItems(items);
+ 			adapter.notifyDataSetChanged();
+ 		}
+ 	};
 }

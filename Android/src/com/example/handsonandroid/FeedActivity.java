77b673cd22d24391bd034a6bfd6991c3db03e345
Garrett <emrickgarrett@gmail.com>
2013-10-04 23:26:48 -0400

    Several changes, User/Events are now classes

--- Android/src/com/example/handsonandroid/FeedActivity.java
+++ Android/src/com/example/handsonandroid/FeedActivity.java
@@ -1,16 +1,22 @@
 package com.example.handsonandroid;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
 
+
+public class FeedActivity extends Activity {
+
+	ArrayList<Events> events;
+	User user;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		getActionBar().hide();
 	}
 
 	@Override
@@ -18,6 +24,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public void onStart(){
+		super.onStart();
 	}
 
 }
---
+++ Android/src/com/example/handsonandroid/User.java
@@ -0,0 +1,19 @@
+package com.example.handsonandroid;
+
+public class User {
+
+	private String id;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String address1;
+	private String address2;
+	private String phone;
+	private String email;
+	
+	public User(){
+		
+	}
+	
+}

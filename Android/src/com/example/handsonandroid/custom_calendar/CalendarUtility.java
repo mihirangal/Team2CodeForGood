22cd4e71b92b20541d0289df18c06ec94f7505a0
Garrett <emrickgarrett@gmail.com>
2013-10-05 04:24:36 -0400

    Changes to some files

--- Android/src/com/example/handsonandroid/custom_calendar/CalendarUtility.java
+++ Android/src/com/example/handsonandroid/custom_calendar/CalendarUtility.java
@@ -4,15 +4,17 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 
+import com.example.handsonandroid.Events;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class CalendarUtility {
-	public static ArrayList nameOfEvent = new ArrayList();
-	public static ArrayList startDates = new ArrayList();
-	public static ArrayList endDates = new ArrayList();
-	public static ArrayList descriptions = new ArrayList();
+	public static ArrayList<String> nameOfEvent = new ArrayList();
+	public static ArrayList<String> startDates = new ArrayList();
+	public static ArrayList<String> endDates = new ArrayList();
+	public static ArrayList<String> descriptions = new ArrayList();
 
 	public static ArrayList readCalendarEvent(Context context) {
 		Cursor cursor = context.getContentResolver()
@@ -30,9 +32,10 @@ public class CalendarUtility {
 		endDates.clear();
 		descriptions.clear();
 		
+		int i = 0;
 		try{
 		
-			for (int i = 0; i < CNames.length; i++) {
+			for (i = 0; i < CNames.length; i++) {
 	
 				nameOfEvent.add(cursor.getString(1));
 				startDates.add(getDate(Long.parseLong(cursor.getString(3))));
@@ -41,9 +44,18 @@ public class CalendarUtility {
 	  			CNames[i] = cursor.getString(1);
 	  			cursor.moveToNext();
 	
-			}
+			} 
 		
 		}catch(Exception ex){}
+		
+		for(int a = 0; a < Events.getList().size(); a++){
+				nameOfEvent.add(Events.getList().get(a).getOpportunityName());
+				startDates.add(Events.getList().get(a).getStartDate());
+				endDates.add(Events.getList().get(a).getEndDate());
+				descriptions.add(Events.getList().get(a).getVolunteerDescription());
+				CNames[i] = Events.getList().get(a).getOpportunityName();
+				i++;
+		}
 		
 		return nameOfEvent;
 	}
